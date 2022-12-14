4504e12ca17a2fe4e347f21395cc74f1c1d46f34
Anand Mahadevan <56053561+AnandMaha@users.noreply.github.com>
2022-12-14 00:15:48 -0800

    Delete dump_png.h

--- dump_png.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DUMP_PNG_H
-#define __DUMP_PNG_H
-
-#include "misc.h"
-
-void Dump_png(Pixel* data,int width,int height,const char* filename);
-void Read_png(Pixel*& data,int& width,int& height,const char* filename);
-
-#endif
